07174cace1b2f505e9b57fac62b5f2a8da67672b
rawsashimi1604 <looweiren@gmail.com>
2024-04-18 10:40:30 +0800

    added simple client architecture and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import "@fontsource/roboto";
+import "@fontsource-variable/inter";
+import "@fontsource-variable/jetbrains-mono";
+import "@fontsource/berkshire-swash";
+import "animate.css";
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/*" element={<NotFound404 />} /> */}
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Main from '../modules/home/Main'
+
+function Home() {
+  return (
+    <Main />
+  )
+}
+
+export default Home
