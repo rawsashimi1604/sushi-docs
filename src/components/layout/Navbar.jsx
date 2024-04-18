78102aafdf47f30dcfccff7c529ba7bcea3dc264
rawsashimi1604 <looweiren@gmail.com>
2024-04-18 13:50:27 +0800

    minor formatting changes

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -3,19 +3,15 @@ import { FaHome } from "react-icons/fa";
 import { useLocation } from "react-router-dom";
 
 function Navbar() {
-  // Get the current path
   const location = useLocation();
-
   useEffect(() => {
-    console.log(location.pathname.split("/"))
+    console.log(location.pathname.split("/"));
   }, [location]);
 
   return (
     <nav className="w-full border-b-[0.2px] pb-3 border-custom-lightWhite tracking-wider font-light">
-      {/* TODO: add some breadcrumbs */}
       <div className="flex flex-row items-center gap-2">
         <FaHome className="text-custom-chaKy h-4 w-4" />
-        {/* TODO: add some dynamic breadcrumbs based on location path  */}
         {location.pathname === "/" ? (
           <span>home</span>
         ) : (
@@ -27,7 +23,7 @@ function Navbar() {
                     <span className="text-custom-chaKy">/</span>
                     <span className="text-sm tracking-widest">{item}</span>
                   </div>
-                )
+                );
               }
             })}
           </>
