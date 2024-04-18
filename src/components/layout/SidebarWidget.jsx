cc4a14add8dd48c2b18565b49fd769742af60bbf
rawsashimi1604 <looweiren@gmail.com>
2024-04-18 12:05:10 +0800

    added simple redirect alert mock for now

--- src/components/layout/SidebarWidget.jsx
+++ src/components/layout/SidebarWidget.jsx
@@ -10,16 +10,21 @@ function SidebarWidget({ contents }) {
       <div
         onClick={() => setIsClicked((prev) => !prev)}
         className={`py-2 px-4 cursor-pointer hover:bg-custom-chaKy hover:text-custom-dark ${
-          isClicked && " bg-custom-chaKy shadow-md text-custom-dark flex gap-1.5 items-center"
+          isClicked &&
+          " bg-custom-chaKy shadow-md text-custom-dark flex gap-1.5 items-center"
         }`}
       >
-        { isClicked && <CiSquareMinus className="h-6 w-6" />}
-        <span onClick={() => alert("redirect")}>{contents.header}</span>
+        {isClicked && <CiSquareMinus className="h-6 w-6" />}
+        <span onClick={() => alert(contents.link)}>{contents.header}</span>
       </div>
       {isClicked && (
         <div className="flex flex-col gap-1 bg-custom-chaKy/90 text-custom-dark">
           {contents.subContents.map((sub) => {
-            return <div className="pl-12 pb-2 pt-2 cursor-pointer hover:bg-custom-chaKy duration-150 transition-all">{sub.header}</div>;
+            return (
+              <div className="pl-12 pb-2 pt-2 cursor-pointer hover:bg-custom-chaKy duration-150 transition-all">
+                <span onClick={() => alert(sub.link)}>{sub.header}</span>
+              </div>
+            );
           })}
         </div>
       )}
