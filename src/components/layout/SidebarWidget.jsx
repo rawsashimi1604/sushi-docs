5770ed9948e90aea2aa68b879106127d14b90009
rawsashimi1604 <looweiren@gmail.com>
2024-04-18 12:04:07 +0800

    update sidebar styles

--- src/components/layout/SidebarWidget.jsx
+++ src/components/layout/SidebarWidget.jsx
@@ -6,20 +6,20 @@ function SidebarWidget({ contents }) {
   const [isClicked, setIsClicked] = useState(false);
 
   return (
-    <div>
+    <div className="tracking-wider text-sm">
       <div
         onClick={() => setIsClicked((prev) => !prev)}
         className={`py-2 px-4 cursor-pointer hover:bg-custom-chaKy hover:text-custom-dark ${
-          isClicked && "  bg-custom-chaKy text-custom-dark flex gap-1.5 items-center"
+          isClicked && " bg-custom-chaKy shadow-md text-custom-dark flex gap-1.5 items-center"
         }`}
       >
         { isClicked && <CiSquareMinus className="h-6 w-6" />}
-        {contents.header}
+        <span onClick={() => alert("redirect")}>{contents.header}</span>
       </div>
       {isClicked && (
-        <div className="pt-2 flex flex-col gap-2 bg-custom-chaKy/80 text-custom-dark">
+        <div className="flex flex-col gap-1 bg-custom-chaKy/90 text-custom-dark">
           {contents.subContents.map((sub) => {
-            return <div className="pl-8 pb-2">{sub.header}</div>;
+            return <div className="pl-12 pb-2 pt-2 cursor-pointer hover:bg-custom-chaKy duration-150 transition-all">{sub.header}</div>;
           })}
         </div>
       )}
