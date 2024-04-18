025c4d822247822d9e066c8e4612553526f69309
rawsashimi1604 <looweiren@gmail.com>
2024-04-18 14:45:39 +0800

    added side mapping of markdown files

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SidebarWidget from "./SidebarWidget";
+import docsMetadata from "../../docs/metadata.json";
 
 const introductionContainer = {
   header: "Introduction",
@@ -25,15 +26,34 @@ const introductionContainer = {
 };
 
 function Sidebar() {
+  const [docs, setDocs] = useState([]);
+
+  useEffect(() => {
+    const mapped = [];
+    for (const item of docsMetadata) {
+      mapped.push({
+        header: item.header,
+        link: item.path,
+        subContents: item.subContents.map((subItem) => ({
+          link: subItem.path,
+          header: subItem.header,
+        })),
+      });
+    }
+    setDocs(mapped);
+  }, []);
+
   return (
     <aside className="min-w-[300px] py-4 h-full border-r-[0.2px] border-custom-lightWhite">
       {/* TODO: insert some logo here. */}
       <div className="px-4">some sushi gateway logo</div>
-      <div className="pb-8 px-4 text-sm text-custom-lightDark font-customMono">latest: v0.0.0</div>
+      <div className="pb-8 px-4 text-sm text-custom-lightDark font-customMono">
+        latest: v0.0.0
+      </div>
       <main className="flex flex-col">
-        <SidebarWidget contents={introductionContainer} />
-        <SidebarWidget contents={introductionContainer} />
-        <SidebarWidget contents={introductionContainer} />
+        {docs.map((mappedDoc) => {
+          return <SidebarWidget contents={mappedDoc} />;
+        })}
       </main>
     </aside>
   );
