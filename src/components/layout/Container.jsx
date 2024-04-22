428ea553800a41c332eaea2e80fd201b4f67138a
rawsashimi1604 <looweiren@gmail.com>
2024-04-22 17:00:46 +0800

    fix css layout

--- src/components/layout/Container.jsx
+++ src/components/layout/Container.jsx
@@ -7,9 +7,9 @@ function Container({ children }) {
     <div className="font-customInter bg-custom-bg text-custom-lightWhite min-w-screen min-h-screen ">
       <main className="flex h-screen">
         <Sidebar />
-        <div className="ml-[300px] w-screen p-4 px-8 h-full">
+        <div className="relative ml-[300px] w-screen h-full">
           <Navbar />
-          <div className="animate__animated animate__fadeIn md:w-[1000px]">{children}</div>
+          <div className="pt-[40px] p-8 animate__animated animate__fadeIn md:w-[1000px]">{children}</div>
         </div>
       </main>
     </div>
--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -9,8 +9,8 @@ function Navbar() {
   // }, [location]);
 
   return (
-    <nav className="w-full border-b-[0.2px] pb-3 border-custom-lightWhite tracking-wider font-light">
-      <div className="flex flex-row items-center gap-2">
+    <nav className="fixed z-10 p-8 pt-4 h-[50px] w-full pb-8 tracking-wider font-light bg-custom-bg">
+      <div className="flex flex-row items-center gap-2 border-b-[0.2px] border-custom-lightWhit pb-2">
         <FaHome className="text-custom-chaKy h-4 w-4" />
         {location.pathname === "/" ? (
           <div className="flex items-center gap-2">
