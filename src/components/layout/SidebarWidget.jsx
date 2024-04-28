ed2930bfb9f62e6e18a49daaa0ec83ca5a26fd84
Gavin Loo <looweiren@gmail.com>
2024-04-28 23:06:46 +0800

    update sidebar cursor pointer for better UX

--- src/components/layout/SidebarWidget.jsx
+++ src/components/layout/SidebarWidget.jsx
@@ -11,12 +11,12 @@ function SidebarWidget({ contents }) {
     <div className="tracking-wider text-sm">
       <div
         onClick={() => setIsClicked((prev) => !prev)}
-        className={`py-2 px-4 cursor-pointer hover:bg-custom-chaKy hover:text-custom-dark ${isClicked &&
+        className={`py-2 px-4 hover:bg-custom-chaKy hover:text-custom-dark ${isClicked &&
           " bg-custom-chaKy shadow-md text-custom-dark flex gap-1.5 items-center"
           }`}
       >
         {isClicked && <CiSquareMinus className="h-6 w-6" />}
-        <span className="font-bold" onClick={() => navigate("/docs" + contents.link)}>{contents.header}</span>
+        <span className="font-bold cursor-pointer" onClick={() => navigate("/docs" + contents.link)}>{contents.header}</span>
       </div>
       {isClicked && (
         <div className="flex flex-col gap-0.5 bg-custom-chaKy/85 text-black">
